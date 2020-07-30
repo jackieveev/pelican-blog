981e9d72b96d76cf6ac7fd7859031aeef732cbc7
jackie.guo <jackie.guo@cloudminds.com>
2020-07-30 18:31:06 +0800

    feat: article页面template

--- themes/paper/static/js/utils.js
+++
@@ -1,9 +0,0 @@
-function onThumbImageLoad (ev) {
-    // console.log(ev.height, ev.width)
-    // if (ev.height > ev.width) {
-    //     ev.style.width = '100%'
-    // } else {
-    //     ev.style.height = '100%'
-    // }
-    // ev.className  = 'visible-item'
-}
